25b441ec3d544621a6108cf7232127aeac2a325b
Nyquist Nwaukwa <nyquist.nwaukwa@gmail.com>
2026-08-30 15:40:43 +0100

    Added the image domain.

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,13 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname:
+          "social-work.3e2d92fb6e61b82cdf636471ee41f62c.r2.cloudflarestorage.com",
+        port: "",
+        pathname: "/resources/**",
+      },
+      {
+        protocol: "https",
         hostname: "pub-ba1e951cf7c545af846050a6519d5f4f.r2.dev",
         port: "",
         pathname: "/courses/**",
@@ -43,6 +50,12 @@ const nextConfig: NextConfig = {
         hostname: "pub-ba1e951cf7c545af846050a6519d5f4f.r2.dev",
         port: "",
         pathname: "/certificate-templates/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pub-ba1e951cf7c545af846050a6519d5f4f.r2.dev",
+        port: "",
+        pathname: "/resources/**",
       },
     ],
   },
